33a8f4adeab593e39598d3c6dcb1f64ed4b967cb
Anisha Dash <anishadash177@gmail.com>
2024-06-17 17:13:40 +0530

    pushing rg folder

---
+++ rg/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "RG" {
+    name = "testRG"
+    location = "west us"
+  
+}
+
+resource "azurerm_resource_group" "RG1" {
+    name = "testRG1"
+    location = "west us"
+  
+}
